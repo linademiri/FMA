f1afbe18c89de616a9e6dc5c4e7b36615992dbed
linademiri <159142830+linademiri@users.noreply.github.com>
2024-06-30 19:20:56 +0200

    js

--- assignments/javascript-interview-final-start/javascript-interview-final-start/assets/js/script.js
+++ assignments/javascript-interview-final-start/javascript-interview-final-start/assets/js/script.js
@@ -1,51 +1,21 @@
-const movieGernes = document.querySelector('.movieGernes');
-const movielist = document.querySelector('.movielist');
-const movieDetailsPage = document.querySelector('.movie-details-page');
-
-console.log(allMovies.length);
-
+const movieThumb = document.querySelector('#movieThumb')
+const allgernes = document.querySelector('#allgernes')
 for (let i = 0; i < allMovies.length; i++) {
-    movieGernes.innerHTML += `
+    allgernes.innerHTML += `
      <option value="${i}">${allMovies[i].gerne}</option>
 `;
 }
-
-loadMovies(0);
-loadMovieDetail(0, 1);
-
-function loadMovies(g) {
-    let allMoviesHTML = '';
-    let gerneMovies = allMovies[g].movies;
-
-    for (let i = 0; i < gerneMovies.length; i++) {
-        allMoviesHTML += `
-        <li class="selected-movie" onclick="loadMovieDetail(${g}, ${i})">
-            <h4>${gerneMovies[i].title}</h4>
-            <img src="assets/img/${gerneMovies[i].thumb}" alt="Plane">
-            <p class="description">${gerneMovies[i].desc}</p>
-            <div class="row movie-stats m0 p0">
-                <div class="col m0 p0">Date: <span>${gerneMovies[i].date}</span> </div>
-                <div class="col m0 p0">Length: <span>${gerneMovies[i].length}</span> </div>
-            </div>
-        </li>
-        `
+function loafMovies(g) {
+    console.log(g);
+    for (let i = 0; i < 8; i++) {
+        movieThumb.innerHTML += `  <li>
+                        <h4>${allgernes[g].movies[i].title}</h4 >
+        <img src="assets/img/ballerina.png" alt="" class="img-xl">
+            <p>Authors</p>
+            <p>desc</p>
+            <p>Length:108min</p>
+            <p>Date:10-20-2023</p>
+        </li>`;
     }
-    movielist.innerHTML = allMoviesHTML;
-}
-
-function loadMovieDetail(g, m) {
-    let currentMovie = allMovies[g].movies[m];
-
-    movieDetailsPage.innerHTML = `
-        <h1>${currentMovie.title}</h1>
-        <h4>Date: ${currentMovie.date} | Length: ${currentMovie.length} min</h4>
-        <div class="container">
-        ${currentMovie.trailer}
-        </div>
-
-        <h4>${currentMovie.actors}</h4>
-        <p>${currentMovie.desc}</p>
-    `;
-
 }
 
